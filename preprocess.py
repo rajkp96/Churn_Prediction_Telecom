7140c0f526b5f28b890d55a99d0faaec984e1c31
rajkp96 <patrarajkumar905@gmail.com>
2026-05-26 11:23:11 +0530

    adding processed data

--- preprocess.py
+++ preprocess.py
@@ -45,3 +45,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 print("Train shape:",X_train.shape)
 print("Test shape:",X_test.shape)
+
+X_train.to_csv("data/X_train.csv",index=False)
+X_test.to_csv("data/X_test.csv",index=False)
+y_train.to_csv("data/y_train.csv",index=False)
+y_test.to_csv("data/y_test.csv",index=False)
