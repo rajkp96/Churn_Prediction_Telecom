e29b05e130e51226c853b5ff4919cda04dee5310
rajkp96 <patrarajkumar905@gmail.com>
2026-05-26 16:41:50 +0530

    adding dockerfile

--- app/model_loader.py
+++ app/model_loader.py
@@ -5,9 +5,9 @@ model=joblib.load("model/churn_model.pkl")
 features=joblib.load("model/features.pkl")
 
 def predict_churn(data:dict):
-    df=pd.dataframe([Data])
+    df=pd.DataFrame([data])
     df=pd.get_dummies(df)
     df=df.reindex(columns=features,fill_value=0)
 
-    prediction = model.predict(df)[o]
+    prediction = model.predict(df)[0]
     return "Yes" if prediction ==1 else "No"
